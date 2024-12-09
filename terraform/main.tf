5bbca7d00129af7413ab2f66fa09a797dae7d3d6
Ayo Phillips <ayophillips@onepipe.io>
2024-12-09 20:26:00 +0100

    fix(terraform): db version

--- terraform/main.tf
+++ terraform/main.tf
@@ -318,7 +318,7 @@ resource "aws_db_instance" "main" {
   allocated_storage   = 20
   storage_type        = "gp2"
   engine              = "postgres"
-  engine_version      = "13.7"
+  engine_version      = "16.3"
   instance_class      = "db.t3.micro"
   db_name             = "appdb"
   username            = "dbadmin"
